10c2ca5bfaac27c36ee528ecab9855a209a035f3
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2022-09-16 19:58:54 -0300

    Add files via upload

--- Exercicio1/src/Main.java
+++ Exercicio1/src/Main.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite a palavra");
-        String palavra = scan.nextLine();
-        int contador = 0;
-        for (int i = 0; i < palavra.length(); i++) {
-            if (Character.isUpperCase(palavra.charAt(i))) {
-                contador++;
+        do {
+            String palavra = scan.nextLine();
+            if (!palavra.equals("FIM")) {
+                int contador = 0;
+                for (int i = 0; i < palavra.length(); i++) {
+                    if (Character.isUpperCase(palavra.charAt(i))) {
+                        contador++;
+                    }
+                }
+                System.out.println(contador);
+            } else {
+                break;
             }
-        }
-        System.out.println(contador);
+        } while (true);
     }
 }
